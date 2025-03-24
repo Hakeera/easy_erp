7bde5d9c3fbcf8581eb11c57132f817e9f04232e
Hakeera <jxhcbhudgfv@gmail.com>
2025-03-24 12:01:11 -0300

    Operações CRUD com gorm

--- configuration/database.go
+++ configuration/database.go
@@ -4,44 +4,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
+	"github.com/Hakeera/easy_erp/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB   *gorm.DB
+	once sync.Once
+)
 
 // InitDB inicializa a conexão com o banco de dados
 func InitDB() {
-	log.Println(os.Getenv("DB_HOST"))
+	once.Do(func() {
+		// Lendo variáveis de ambiente
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbName := os.Getenv("DB_NAME")
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
 
-	// Lendo variáveis de ambiente
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+		// Criando a string de conexão
+		dsn := fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			dbHost, dbUser, dbPassword, dbName, dbPort,
+		)
 
-	// Criando a string de conexão
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		dbHost, dbUser, dbPassword, dbName, dbPort,
-	)
+		// Conectando ao banco de dados
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+		}
 
-	// Conectando ao banco de dados
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
-	}
+		// Criando a tabela automaticamente
+		err = DB.AutoMigrate(&model.Client{})
+		if err != nil {
+			log.Fatalf("Erro ao migrar a tabela: %v", err)
+		}
 
-	log.Println("Conexão com o banco de dados estabelecida com sucesso")
+		log.Println("Conexão com o banco de dados estabelecida com sucesso")
+	})
 }
 
 // GetDB retorna a instância do banco de dados já inicializada
 func GetDB() *gorm.DB {
 	if DB == nil {
-		log.Fatal("Banco de dados não foi inicializado! Certifique-se de chamar InitDB() antes de usar GetDB().")
+		log.Fatal("Banco de dados não foi inicializado!") 
 	}
-	return DB 
+	return DB
 }
+
--- service/clients_service.go
+++ service/clients_service.go
@@ -1,47 +1,55 @@
-package services
+package service
 
 import (
 	"errors"
 
 	"github.com/Hakeera/easy_erp/configuration"
-	models "github.com/Hakeera/easy_erp/model"
+	"github.com/Hakeera/easy_erp/model"
 )
 
-func CreateClient(client *models.Client) error {
-	result := configuration.DB.Create(client)
-	if result.Error != nil {
-		return result.Error
+// Criar Cliente
+func CreateClient(client *model.Client) error {
+	db := configuration.GetDB()
+	if err := db.Create(client).Error; err != nil {
+		return err
 	}
 	return nil
 }
 
-func GetClients() ([]models.Client, error) {
-	var clients []models.Client
-	result := configuration.DB.Find(&clients)
-	if result.Error != nil {
-		return nil, result.Error
+// Buscar todos os clientes
+func GetClients() ([]model.Client, error) {
+	db := configuration.GetDB()
+	var clients []model.Client
+	if err := db.Find(&clients).Error; err != nil {
+		return nil, err
 	}
 	return clients, nil
 }
 
-func UpdateClient(id uint, updatedData *models.Client) error {
-	var client models.Client
-	result := configuration.DB.First(&client, id)
-	if result.Error != nil {
+// Atualizar Cliente
+func UpdateClient(id uint, updatedData *model.Client) error {
+	db := configuration.GetDB()
+	var client model.Client
+
+	if err := db.First(&client, id).Error; err != nil {
 		return errors.New("cliente não encontrado")
 	}
 
+	// Atualizando os dados
 	client.Name = updatedData.Name
 	client.Email = updatedData.Email
 	client.Phone = updatedData.Phone
 
-	configuration.DB.Save(&client)
+	if err := db.Save(&client).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
+// Deletar Cliente
 func DeleteClient(id uint) error {
-	result := configuration.DB.Delete(&models.Client{}, id)
-	if result.Error != nil {
+	db := configuration.GetDB()
+	if err := db.Delete(&model.Client{}, id).Error; err != nil {
 		return errors.New("erro ao excluir cliente")
 	}
 	return nil
